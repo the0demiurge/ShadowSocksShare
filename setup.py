4cde297ce6a71647842d95ac207c26b08105a103
Template builder <builder@example.com>
2017-08-24 22:42:01 -0400

    Creating template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
+     )
