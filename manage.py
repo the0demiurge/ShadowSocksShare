2f10bdd46e5ab7e853c114510854a861b44ae42c
Charles Xu <charl3s.xu@gmail.com>
2017-08-28 10:31:52 +0800

    changed port to bind PORT environ variable

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- utf-8 -*-
 
+import os
 from flask_script import Manager, Server
 from app import app
+
+port = os.environ.get('PORT')
 
 manager = Manager(app)
 
 manager.add_command(
-    "runserver", Server(host="0.0.0.0", port=34546, use_debugger=True)
+    "runserver", Server(host="0.0.0.0", port=port, use_debugger=True)
 )
 
 if __name__ == '__main__':
