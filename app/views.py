0bddde2eb5215fb6a9b8da79b9756858b33bd324
Charles Xu <charl3s.xu@gmail.com>
2017-08-28 10:27:13 +0800

    testing changing port

--- app/views.py
+++ app/views.py
@@ -36,7 +36,7 @@ def update_servers():
     encoded = base64.urlsafe_b64encode(bytes(decoded, 'utf-8'))
 
 
-counter_path = os.path.expanduser('counter')
+counter_path = os.path.expanduser('/tmp/counter')
 
 
 def counter(counter_path=counter_path):
