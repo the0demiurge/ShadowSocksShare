31344979d476537571096acd0ec5804c05a8a457
Charles Xu <charl3s.xu@gmail.com>
2017-08-31 10:45:11 +0800

    re-added support to openshift ^_^

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- utf-8 -*-
+from app import app as application
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=8051)
+    # from wsgiref.simple_server import make_server
+    # httpd = make_server('localhost', 8051, application)
+    # httpd.serve_forever()
