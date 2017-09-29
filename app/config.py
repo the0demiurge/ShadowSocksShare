9f155484bbaae989705a8b7d143ca213f9ede49f
Charles Xu <charl3s.xu@gmail.com>
2017-09-29 13:32:46 +0800

    去掉了两个没用的源

--- app/config.py
+++ app/config.py
@@ -5,11 +5,9 @@ url = [
     # 'https://plus.google.com/communities/103542666306656189846/stream/dd570c04-df51-4394-8c83-eabb12cc0d0c',
     # 'https://plus.google.com/communities/106442142549456855872/stream/62937124-015f-4f9c-81c9-914444653b8a',
     # 'https://plus.google.com/communities/104092405342699579599/stream/8a593591-2091-4096-bb00-7d9c5659db93',
-    'http://gdmi.weebly.com/3118523398online.html',
     # 'https://plus.google.com/communities/104092405342699579599/posts/105945645464453975098',
     'https://plus.google.com/communities/104092405342699579599/stream/8a593591-2091-4096-bb00-7d9c5659db93',
     'https://plus.google.com/communities/109204483693184691558',
     'https://plus.google.com/communities/117702818760720009772/stream/47c69db4-9362-4d91-a017-97f3be948437',
-    'https://plus.google.com/communities/107236330951218376310/stream/4d9a88f4-1fe1-4466-9cfb-b45f28353d10',
     'https://plus.google.com/communities/107859708371989171939',
 ]
