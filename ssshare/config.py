e5281e54bb7579b3b5f4d368874364ed1d4668d0
Charles Xu <charl3s.xu@gmail.com>
2019-06-03 15:45:06 +0800

    added some new list

--- ssshare/config.py
+++ ssshare/config.py
@@ -3,8 +3,10 @@
 url = [
     'http://www.52ssr.net/',
     'https://t.me/s/SSRlist',
+    'https://t.me/s/SSRSUB',
 ]
 
 subscriptions = [
-    'https://raw.githubusercontent.com/AmazingDM/sub/master/ssrshare.com'
+    'https://raw.githubusercontent.com/AmazingDM/sub/master/ssrshare.com',
+    'https://raw.githubusercontent.com/ssrsub/ssr/master/ssrsub',
 ]
