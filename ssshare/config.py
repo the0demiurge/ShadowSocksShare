a54574918923d5c5a31ba609e14a501ae89acfa6
Dct Mei <dctxmei@gmail.com>
2019-09-12 19:54:41 +0900

    add a new url: nulastudio.org

--- ssshare/config.py
+++ ssshare/config.py
@@ -8,6 +8,7 @@ url = [
     'https://fanqiang.network/free-shadowsocksr-accounts-page-5',
     'https://github.com/Steve-ShadowsocksR/-SSR-/blob/master/%E5%B0%8F%E9%A3%9E%E6%9C%BA%E7%BA%AF%E5%85%AC%E7%9B%8ASSR%E5%88%86%E4%BA%AB%EF%BC%81',
     'https://lncn.org/',
+    'https://nulastudio.org/Freedom/',
     'https://t.me/s/gyjclub',
     'https://t.me/s/SSRlist',
     'https://t.me/s/SSRSUB',
