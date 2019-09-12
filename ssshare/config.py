91caadfe19b14e90989446a25d3babf5968e7730
Dct Mei <dctxmei@gmail.com>
2019-09-12 19:13:31 +0900

    add a new url: youneed.win

--- ssshare/config.py
+++ ssshare/config.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 url = [
-    'http://www.52ssr.net/',
-    'https://t.me/s/SSRlist',
-    'https://t.me/s/SSRSUB',
-    'https://t.me/s/gyjclub',
     'https://fanqiang.network/',
     'https://fanqiang.network/free-shadowsocksr-accounts-page-2',
     'https://fanqiang.network/free-shadowsocksr-accounts-page-3',
     'https://fanqiang.network/free-shadowsocksr-accounts-page-4',
     'https://fanqiang.network/free-shadowsocksr-accounts-page-5',
     'https://github.com/Steve-ShadowsocksR/-SSR-/blob/master/%E5%B0%8F%E9%A3%9E%E6%9C%BA%E7%BA%AF%E5%85%AC%E7%9B%8ASSR%E5%88%86%E4%BA%AB%EF%BC%81',
+    'https://t.me/s/gyjclub',
+    'https://t.me/s/SSRlist',
+    'https://t.me/s/SSRSUB',
+    'https://youneed.win/free-ss',
+    'https://youneed.win/free-ssr',
+    'http://www.52ssr.net/',
 ]
 
 subscriptions = [
